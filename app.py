711755c03a8e3f819bd4d5bb9e30e6067bdc0f15
Test-Rohit1960 <166794692+Test-Rohit1960@users.noreply.github.com>
2024-05-10 16:26:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello():
-    return "<h2>Hello world - Integration Activity Flask Deployment Lab: try 2 <h2><hr/>"\
+    return "<h2>Hello world - Integration Activity Flask Deployment Lab: try 3 <h2><hr/>"\
 
 
 app.run(host="0.0.0.0", port=5000)
